ba1c124bd09ce3327e0649b408a875725f6245bb
harshita26s <145872843+harshita26s@users.noreply.github.com>
2024-06-28 18:37:29 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newrer version  
+
+// alert (3+3) // we are using node.js, not browser
+
+console.log(3
+    +
+     3)  // code readability should be high
+
+console.log("harshita")     
+
+
+let name = "harshi"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bright
+// string = ""
+// boolean = true/false
+// null = standalone value 
+// undefined =
+// symbol = unique 
+
+// object 
+
+console.log(typeof "harshi");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.table([typeof "harshi", typeof null, typeof age, typeof undefined])
