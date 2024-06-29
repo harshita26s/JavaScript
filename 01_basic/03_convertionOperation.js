68497eec3074f2a09249ec70957cac413b6329f3
harshita26s <145872843+harshita26s@users.noreply.github.com>
2024-06-29 16:14:23 +0000

    operations

--- 01_basic/03_convertionOperation.js
+++ 01_basic/03_convertionOperation.js
@@ -29,3 +29,34 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *************************************operatoions***************************************
+
+let str1 = "Hello"
+let str2 = " Harshi"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+// ++gamecounter;  //101   prefix
+gamecounter++;  //101    Postfix
+
+console.log(gamecounter);
