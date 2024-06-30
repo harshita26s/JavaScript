826485f3ff8c047ac70f74bf9ce9656e26694647
harshita26s <145872843+harshita26s@users.noreply.github.com>
2024-06-30 13:31:51 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// following dataType changing ..not used in typescript
+// console.log("2" > 1);  //true
+// console.log("02" > 1);  //true
+
+// following type of conversion can cause trouble..
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+//  ===
+
+console.log("2" === 2);  
